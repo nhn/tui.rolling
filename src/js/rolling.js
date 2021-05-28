ceb1a5dc8a57c6955a9778ba7f8f27840bdea2a2
jajugoguma <jajugoguma@gmail.com>
2021-05-28 16:19:06 +0900

    chore: change comments of example

--- src/js/rolling.js
+++ src/js/rolling.js
@@ -32,9 +32,14 @@ var Data = require('./rolldata');
  *      @param {String} [options.panelTag='li.className'] A tag name for panel, connect tag name with class by dots [default value is li]
  * @param {Array|String} data A data of rolling panels
  * @example
- * import Rolling from 'tui-rolling'; // ES6
- * // const Rolling = require('tui-rolling'); // CommonJS
- * // const Rolling = tui.Rolling;
+ * // ES6
+ * import Rolling from 'tui-rolling';
+ *
+ * // CommonJS
+ * const Rolling = require('tui-rolling');
+ *
+ * // Browser
+ * const Rolling = tui.Rolling;
  *
  * const instance = new tui.Rolling({
  *      element: document.getElementById('rolling'),
