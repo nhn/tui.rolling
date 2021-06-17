a6cd34f0c30c8bd0a7f699c06f3b5b1d2e7277f2
jajugoguma <jajugoguma@gmail.com>
2021-06-17 18:07:39 +0900

    chore: set jest test environment
    chore: remove karma-jasmine test environment

---
+++ test/setup-globals.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line strict
+var path = require('path');
+var fs = require('fs');
+var fixturesPath = './fixtures';
+var Inliner = require('css-inline');
+
+global.loadFixtures = function(fileName) {
+  var data;
+  var dir = path.resolve(__dirname, fixturesPath, fileName);
+
+  try {
+    // eslint-disable-next-line no-sync
+    data = fs.readFileSync(dir, 'utf8');
+    document.body.innerHTML = data;
+  } catch (err) {
+    document.body.innerHTML = '';
+  }
+};
+
+global.loadStyleFixtures = function(fileName) {
+  var data;
+  var dir = path.resolve(__dirname, fixturesPath, fileName);
+
+  try {
+    // eslint-disable-next-line no-sync
+    data = fs.readFileSync(dir, 'utf8');
+    document.body.innerHTML = Inliner.inline(
+      '<style>' + data + '</style>' + document.body.innerHTML,
+      { remove_style_tags: true }
+    );
+    // eslint-disable-next-line no-empty
+  } catch (err) {}
+};
